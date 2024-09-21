1d11df93c6aa2a8d79a39c29ef476ddab63f1d2c
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2024-09-21 16:22:20 -0400

    Navigation part

---
+++ food-delivery-app/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { view, Text } from 'react-native'
+import React from 'react';
+
+export default function HomeScreen() {
+    return(
+        <view>
+            <Text>HomeScreen</Text>
+        </view>
+    )
+}
---
+++ food-delivery-app/screens/RestaurantScreen.js
@@ -0,0 +1,10 @@
+import { view, Text } from 'react-native'
+import React from 'react';
+
+export default function RestaurantScreen() {
+    return(
+        <view>
+            <Text>RestaurantScreen</Text>
+        </view>
+    )
+}
